6963d69af49c84cd554e6a6ace60ba2f17e56a6a
pintofelipe <afpintou@ufpso.edu.co>
2023-10-18 12:37:37 -0500

    Add exercise Solving for Carrots (Java)

---
+++ Java/Easy/Solving for Carrots.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+ class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numberOne = sc.nextInt();
+        int numberTwo = sc.nextInt();
+        String word = sc.next();
+        
+        System.out.println(numberTwo);
+        
+    }
+    
+    
+
+}

6905aea256aea13b6ad41b0b0ffd27e38756c9d8
pintofelipe <afpintou@ufpso.edu.co>
2023-10-18 12:39:30 -0500

    Add exercise Óvissa (Java)

---
+++ Java/Easy/Óvissa.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Kattis {
+
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        
+        String word = sc.next();
+        
+        int c = word.length();
+        if(c>0){
+            System.out.println(c);
+        }
+    }
+}

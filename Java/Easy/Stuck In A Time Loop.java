4e566361297da4c0b814a53864b95d6846d5a60e
pintofelipe <afpintou@ufpso.edu.co>
2023-10-18 12:38:28 -0500

    Add exercise Stuck In A Time Loop (Java)

---
+++ Java/Easy/Stuck In A Time Loop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+ class Main {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+        int number = sc.nextInt();
+        
+        for (int i = 0; i < number; i++) {
+            System.out.println((i+1)+" Abracadabra");
+        }
+        
+    }
+}

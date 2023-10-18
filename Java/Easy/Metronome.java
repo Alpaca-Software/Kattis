7f2d6ed89abb45ac284839b5d97228a17af4c5cc
pintofelipe <afpintou@ufpso.edu.co>
2023-10-18 12:31:34 -0500

    Add exercise Leggja metroneme (java)

---
+++ Java/Easy/Metronome.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Metronome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        float number = sc.nextFloat();
+        
+        float vueltas = number/4;
+        
+        System.out.println(vueltas);
+      
+    }
+}

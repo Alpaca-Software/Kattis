d9382bd5cb46dfe4e5e682156c0d188419173443
pintofelipe <afpintou@ufpso.edu.co>
2023-10-18 12:28:01 -0500

    Add exercise juxtaposition (java)

---
+++ Java/Easy/Jack-O'-Lantern Juxtaposition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int numberOne = sc.nextInt();
+        int numberTwo = sc.nextInt();
+        int numberThree = sc.nextInt();
+        
+        int result = numberOne*numberTwo*numberThree;
+        System.out.println(result);
+        
+    }
+}

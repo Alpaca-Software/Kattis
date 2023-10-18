cccb42b9b5c5b6e8534a20e848c74408cc3542e3
pintofelipe <afpintou@ufpso.edu.co>
2023-10-18 12:30:39 -0500

    Add exercise knight (java)

---
+++ Java/Easy/Knight Packing.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Knight {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int number = sc.nextInt();
+        
+        if(number%2==0){
+            System.out.println("second");
+        }else{
+            System.out.println("first");
+        }
+           
+      
+    }
+}

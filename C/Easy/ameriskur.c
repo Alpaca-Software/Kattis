41a63c465fe273105b3fc0975188b94feb063a0d
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-11-07 12:33:37 -0500

    Add exercise Amerískur vinnustaður (C)

---
+++ C/Easy/ameriskur.c
@@ -0,0 +1,14 @@
+// Pass.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int length;
+
+  scanf("%d", &length);
+
+  printf("%f\n", length * 0.09144);
+
+  return EXIT_SUCCESS;
+}
